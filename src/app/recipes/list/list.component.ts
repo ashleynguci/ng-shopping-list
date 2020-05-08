92000be576e4394eb5e96c164eefe24a39cb19ef
ashleynguci23 <backupmacbook2305@gmail.com>
2020-05-08 17:06:41 +0700

    adding function to app|

--- src/app/recipes/list/list.component.ts
+++ src/app/recipes/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,10 +9,14 @@ import { Recipe } from '../recipe.model';
 export class ListComponent implements OnInit {
   recipes: Recipe[] = [new Recipe("Banh xeo", "a Vietnamese pancake ", "https://cdn.daotaobeptruong.vn/wp-content/uploads/2020/01/banh-xeo-mien-tay.jpg"), new Recipe("Banh cuon", "a Vietnamese spring roll ", "https://lh3.googleusercontent.com/proxy/6peHFIV7d5RqaSDVdm1y7EsymJ3gT580My-uJSP8LNFyNpDaR8-5sVXOBxUT7nltyGppnaXRGt9WU0JEPNwZBilJfzmjUjH7JBh3CVvla4VcD7gugD4vYPzs2cM1T8DI9_9AvExzGR3ru3bmJW7EIybuVXOUP1UtgSbbXJ8YQz1verzdxSJJchCV_ioiQw5F5ChEhA9VpDOTE_vxYqsKLegOerch3jrEIviZ")];
 
-
+  @Output() recipeChoosen = new EventEmitter<Recipe>();
+  // Because now pass to parent component
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeChoosen.emit(recipe);
+  }
 }
--- src/app/recipes/list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,10 +8,14 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
 
-  @Input() recipe: Recipe
+  @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
