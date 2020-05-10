50675dc92a0b006d400ee63d48969e49dafa524a
Thỏ Ngọc <ashley@Minhs-MacBook-Pro-7.local>
2020-05-10 16:07:53 +0700

    customize README.

--- src/app/recipes/list/list.component.ts
+++ src/app/recipes/list/list.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  recipes: Recipe[] = [new Recipe("Banh xeo", "a Vietnamese pancake ", "https://cdn.daotaobeptruong.vn/wp-content/uploads/2020/01/banh-xeo-mien-tay.jpg"), new Recipe("Banh cuon", "a Vietnamese spring roll ", "https://lh3.googleusercontent.com/proxy/6peHFIV7d5RqaSDVdm1y7EsymJ3gT580My-uJSP8LNFyNpDaR8-5sVXOBxUT7nltyGppnaXRGt9WU0JEPNwZBilJfzmjUjH7JBh3CVvla4VcD7gugD4vYPzs2cM1T8DI9_9AvExzGR3ru3bmJW7EIybuVXOUP1UtgSbbXJ8YQz1verzdxSJJchCV_ioiQw5F5ChEhA9VpDOTE_vxYqsKLegOerch3jrEIviZ")];
+  recipes: Recipe[] = [new Recipe("Banh xeo", "a Vietnamese pancake ", "https://cdn.daotaobeptruong.vn/wp-content/uploads/2020/01/banh-xeo-mien-tay.jpg"), new Recipe("Banh cuon", "a Vietnamese spring roll ", "https://www.hoidaubepaau.com/wp-content/uploads/2019/06/hoc-lam-banh-cuon-mo-quan.jpg")];
 
   @Output() recipeChoosen = new EventEmitter<Recipe>();
   // Because now pass to parent component
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter,Output } from '@angular/core';
+import { Ingredient } from 'src/app/recipes/share/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+@ViewChild('nameInput') nameInputRef: ElementRef;
+@ViewChild('amountInput') amountInputRef: ElementRef;
+
+@Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem(){
+    console.log(this.nameInputRef);
+  }
 }
