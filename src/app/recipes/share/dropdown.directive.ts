ee75c310752cb3020270fb40633e65229d6ddf0b
ashleynguci23 <backupmacbook2305@gmail.com>
2020-05-11 08:19:40 +0700

    add close dropdown

--- src/app/recipes/share/dropdown.directive.ts
+++ src/app/recipes/share/dropdown.directive.ts
@@ -1,13 +1,12 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
-
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
-    selector:'[appDropdown]'
+    selector: '[appDropdown]'
 })
-export class DropdownDirective{
-   @HostBinding('class.open') isOpen = false;
-
-@HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
-}
+export class DropdownDirective {
+    @HostBinding('class.open') isOpen = false;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    }
+    constructor(private elRef: ElementRef) { }
 }
