98f0bee5d9efd9f582ee81b78990f0824b4a4489
ashleynguci23 <backupmacbook2305@gmail.com>
2020-05-12 11:03:42 +0700

    add delete ingredient func

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -55,6 +55,10 @@ export class RecipeEditComponent implements OnInit {
 
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImgUrl = '';
