3d5e342649a2ed2d168dd73cf2278c670703d79f
ashleynguci23 <backupmacbook2305@gmail.com>
2020-05-13 09:57:42 +0700

    adding auth page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 
@@ -18,7 +19,8 @@ const appRoutes: Routes = [
             { path: ':id', component: RecipeDetailComponent },
             { path: ':id/edit', component: RecipeEditComponent }]
     },
-    { path: 'shopping-list', component: ShoppingListComponent }
+    { path: 'shopping-list', component: ShoppingListComponent },
+    { path: 'app-auth', component: AuthComponent }
 ]
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
